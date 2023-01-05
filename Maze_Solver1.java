b849b2042b21234e7df0b4a49ffe9c459ff12357
Rukmini-Challa <120375689+Rukmini-Challa@users.noreply.github.com>
2023-01-05 10:34:30 +0530

    Add files via upload

---
+++ Maze_Solver1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.maze_solver1;
+
+/**
+ *
+ * @author USER
+ */
+public class Maze_Solver1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
